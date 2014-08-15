5900d60499286cee89891d817f7b43d2477d2fa0
Junya Ogura <junyaogura@gmail.com>
2014-08-16 00:12:18 +0900

    Add User.from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,54 @@ class User < ActiveRecord::Base
   validates :username, presence: true, length: { in: 1..255 }
   validates :name, presence: true, length: { in: 2..20 }
   validates :avatar_url, presence: true, length: { maximum: 255 }
+
+  # Public: Returns an instance of User which corresponds to
+  # specified auth hash.
+  #
+  # If an user does not exist, it will create.
+  # Attributes of the User will update with auth values.
+  #
+  # auth - An instance of OmniAuth::AuthHash.
+  #
+  # Returns User.
+  def self.from_omniauth(auth)
+    user = where(auth.slice('provider', 'uid')).first
+    user ? update_user!(user, auth) : create_from_omniauth!(auth)
+  end
+
+  private
+
+  # Internal: Creates a new user with specified auth.
+  #
+  # auth - An instance of OmniAuth::AuthHash.
+  #
+  # Returns User.
+  def self.create_from_omniauth!(auth)
+    attrs = {
+      provider: auth['provider'],
+      uid: auth['uid'],
+      username: auth['info']['nickname'],
+      name: auth['info']['name'],
+      avatar_url: auth['info']['image'],
+    }
+    create!(attrs)
+  end
+
+  # Internal: Update specified user with auth.
+  #
+  # user - An instance of User to update.
+  # auth - An instance of OmniAuth::AuthHash.
+  #
+  # Returns User.
+  def self.update_user!(user, auth)
+    attrs = {
+      username: auth['info']['nickname'],
+      name: auth['info']['name'],
+      avatar_url: auth['info']['image'],
+    }
+    user.update!(attrs)
+    user
+  end
 end
 
 # == Schema Information
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  def auth_mock
+    {
+      'provider' => 'github',
+      'uid' => '1',
+      'info' => {
+        'nickname' => 'foo',
+        'name' => 'Foo Bar',
+        'image' => 'http://example.com/image.jpg',
+      },
+    }
+  end
+
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:provider) }
     it { is_expected.to validate_presence_of(:uid) }
@@ -24,5 +36,55 @@ RSpec.describe User, type: :model do
     }
     it { is_expected.to validate_presence_of(:avatar_url) }
     it { is_expected.to ensure_length_of(:avatar_url).is_at_most(255) }
+  end
+
+  describe '.from_omniauth' do
+    context 'user exists' do
+      before do
+        @auth = auth_mock
+
+        # Creates user corresponds with the auth
+        @user = FactoryGirl.create(
+          :user,
+          provider: @auth['provider'],
+          uid: @auth['uid'],
+          username: 'test',
+          name: 'Test',
+          avatar_url: 'http://example.com/test.jpg',
+        )
+      end
+
+      it 'updates the user with values of auth' do
+        # User receives update! call
+        attrs = {
+          username: 'foo',
+          name: 'Foo Bar',
+          avatar_url: 'http://example.com/image.jpg',
+        }
+        expect_any_instance_of(User).to receive(:update!).with(attrs)
+
+        described_class.from_omniauth(auth_mock)
+      end
+
+      it 'returns an instance of User' do
+        expect(described_class.from_omniauth(auth_mock)).to be_kind_of(User)
+      end
+    end
+
+    context 'user does not exist' do
+      it 'creates a new user' do
+        expect {
+          described_class.from_omniauth(auth_mock)
+        }.to change(User, :count).by(1)
+      end
+
+      it 'returns an instance of User' do
+        expect(described_class.from_omniauth(auth_mock)).to be_kind_of(User)
+      end
+
+      it 'returns a persisted object' do
+        expect(described_class.from_omniauth(auth_mock)).to be_persisted
+      end
+    end
   end
 end
