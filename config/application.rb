f23b1fdff774a973135d71077185ba7c7b545f5b
Junya Ogura <junyaogura@gmail.com>
2014-08-16 00:18:49 +0900

    Update application configs

--- config/application.rb
+++ config/application.rb
@@ -1,30 +1,27 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Whovar
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
+    config.assets.version = '20140816'
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.generators.assets      = false
+    config.generators.helper      = false
+    config.generators.javascripts = false
+    config.generators.stylesheets = false
+    config.generators.view_tests  = false
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
